859382769fa16ef6fadb6c5f34f94b3a814af25d
NicoleGruber <57449553+NicoleGruber@users.noreply.github.com>
2020-01-06 11:49:01 -0300

    Create HBSIS.py
    Hbsis AirLines

---
+++ Hbsis AirLines/HBSIS.py
@@ -0,0 +1,106 @@
+from time import sleep
+el1='Piloto'
+el2='Oficial 1'
+el3='Oficial 2'
+el4='chefe de serviço de voo'
+el5='comissária 1'
+el6='comissária 2'
+el7='Bandido'
+el8='Policial'
+carro = []
+aviao = []
+terminal = [el1,el2,el3,el4,el5,el6,el7,el8]
+numero_viagem=0
+
+def viagem_fortwo():
+    global numero_viagem
+    numero_viagem += 1
+    print(f'\n======= Viagem {numero_viagem} =======')
+    sleep(2)
+    print(f'Estão no terminal: {terminal}')
+    sleep(2)
+
+print(carro)
+
+for viagem in range(1,8):
+    if viagem == 1:
+        viagem_fortwo()
+        terminal.remove(el1)
+        terminal.remove(el5)
+        carro.append(el1)
+        carro.append(el5)
+        print(f'A {el5} e o {el1} embarcaram no Fortwo')
+        sleep(2)
+        carro.remove(el5)
+        aviao.append(el5)
+        print(f'A {el5} desce do Fortwo e embarca no avião ')
+        sleep(2)
+        print(f'Estão no avião: {aviao}')
+        sleep(2)
+        print('O piloto volta no Fortwo para o terminal')
+        sleep(2)    
+    elif viagem == 2:
+        viagem_fortwo()
+        terminal.remove(el6)
+        carro.append(el6)
+        print('A comissária 2 embarca no Fortwo com o Piloto')
+        sleep(2)
+        carro.remove(el6)
+        aviao.append(el6)
+        print('A comissária desce do Fortwo e embarca no avião')
+        sleep(2) 
+        print(f'Estão no avião: {aviao}')
+        sleep(2) 
+        print('O piloto volta no Fortwo para o terminal')
+        sleep(2)
+    elif viagem == 3:
+        viagem_fortwo()
+        carro.remove(el1)
+        print('O piloto desce do Fortwo e fica no terminal')
+        sleep(2)
+        carro.append(el4)
+        carro.append(el2)
+        print(f'{el4} e {el2} entram no Fortwo e vão até o avião')
+        sleep(2)
+        carro.remove(el2)
+        aviao.append(el2)
+        print(f'O {el2} entra no avião')
+        sleep(2)
+        print(f'Estão no avião: {aviao}')
+        sleep(2)
+        print(f'O {el4} volta no Fortwo para o terminal')
+        sleep(2)
+    elif viagem == 4:
+        viagem_fortwo()
+        carro.append(el3)
+        print(f'{el3} entra no Fortwo com o {el4} e vão até o avião')
+        sleep(2)
+        carro.remove(el3)
+        aviao.append(el3)
+        print(f'O {el3} entra no avião')
+        sleep(2)
+        print(f'Estão no avião: {aviao}')
+        sleep(2)
+        print(f'O {el4} volta no Fortwo para o terminal')
+        sleep(2)
+    elif viagem == 5:
+        viagem_fortwo()
+        carro.remove(el4)
+        print(f'O {el4} sai do fortwo e fica no terminal')
+        sleep(2)
+        carro.append(el8)
+        carro.append(el7)
+        print(f'O {el7} e o {el8} entram no fortwo e vão até o avião')
+        terminal.remove(el7)
+        terminal.remove(el8)
+        sleep(2)
+        carro.remove(el7)
+        carro.remove(el8)
+        aviao.append(el8)
+        aviao.append(el7)
+        print(f'O {el7} e o {el8} saem do fortwo e entram no avião')
+        sleep(2)
+        print(f'O {el3} sai do avião e volta com o fortwo')
+        sleep(2)
+        print(f'Estão no avião: {aviao}')
+
